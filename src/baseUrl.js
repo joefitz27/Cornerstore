f48e0808f4320d8bef91470228204567663209e5
sangram9693 <sangram.bachu@gmail.com>
2019-04-14 01:12:57 +0530

    simple commit

--- src/baseUrl.js
+++ src/baseUrl.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'https://powerful-river-91889.herokuapp.com/';
+export const BASE_URL = 'http://localhost:4000/api/';
 export const IMAGE_URL = 'http://localhost:4000/product_image/';
